d7f1fc9a438abf18e12c2d78e909a64a3a9848a9
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-27 15:38:57 +0530

    added brain script

--- Brain.cs
+++ Brain.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class Brain : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int DNALength = 2;
+    public DNA dna;
+    public GameObject eyes;
+    bool seeWall = true;
+    Vector3 startPosition;
+    public float distanceTravelled = 0;
+    bool alive = true;
+
+    public void Init()
     {
-        
+        // init DNA
+        // 0 forward
+        // 1 Angle turn
+        dna = new DNA(DNALength, 360);
+        startPosition = this.transform.position;
     }
 
+     void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "dead")
+        {
+            distanceTravelled = 0;
+            alive = false;
+        }
+    }
+
+
+ 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!alive) return;
+
+        seeWall = false;
+        RaycastHit hit;
+        Debug.DrawRay(eyes.transform.position, eyes.transform.forward * 0.5f, Color.magenta);
+        if(Physics.SphereCast(eyes.transform.position,0.1f,eyes.transform.forward,out hit, 0.5f))
+        {
+            if (hit.collider.gameObject.tag == "wall")
+            {
+                seeWall = true;
+            }
+        }
+    }
+
+     void FixedUpdate()
+    {
+        if (!alive) return;
+
+        //read dna
+        float h = 0;
+        float v = dna.GetGene(0);
+
+        if (seeWall)
+        {
+            h = dna.GetGene(1);
+        }
+
+        this.transform.Translate(0, 0, v * 0.001f);
+        this.transform.Rotate(0, h, 0);
+        distanceTravelled = Vector3.Distance(startPosition, this.transform.position);
     }
 }
