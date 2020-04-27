0b3e64a0c2d64c23de98fd780e278772bf6dc7d6
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-27 15:01:05 +0530

    initial commit

---
+++ GenerateMaze.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMaze : MonoBehaviour {
+
+	public GameObject blockPrefab;
+	int width = 40;
+	int depth = 40;
+
+	// Use this for initialization
+	void Awake () {
+		for(int w = 0; w < width; w++)
+		{
+			for(int d = 0; d < depth; d++)
+			{
+				if( w == 0 || d == 0)	//outside walls bottom and left
+				{
+					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y, d + this.transform.position.z), Quaternion.identity);
+				}	
+				else if( w < 3 && d < 3)
+				{
+					continue;
+				}		
+				else if(w == width-1 || d == depth-1) //outside walls top and right
+				{
+					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity);
+				}
+				else if(Random.Range(0,5) < 1)
+				{
+					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity);
+				}
+			}	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
